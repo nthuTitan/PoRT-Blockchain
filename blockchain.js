5a306a353bd4c4e8bf75830756e805f8af7caa2b
nthuTitan <43961291+nthuTitan@users.noreply.github.com>
2020-10-07 23:44:42 +0800

    Add files via upload

---
+++ blockchain.js
@@ -0,0 +1,182 @@
+const uuid = require('uuid/v1');
+const currentNodeUrl = process.argv[3];
+
+// local modules
+const Block = require('./block.js')
+const Txn_Pool = require('./transaction_pool');
+
+const TRANSACTION_TYPE = {
+    transaction: "TRANSACTION",
+    stake: "STAKE",
+    validator_fee: "VALIDATOR_FEE"
+};
+
+
+function Blockchain(){
+    this.chain = [];
+    // this.pendingTransactions = [];
+
+    this.currentNodeUrl = currentNodeUrl;
+    this.networkNodes = [];
+  
+    // this.accounts = new Account();
+    // this.address = this.accounts.getAddress();
+    var txn_pool = new Txn_Pool();
+    txn_pool.create(1);
+    var genesisBlock = new Block(4000718, txn_pool.transactions, '0xa3d2f1958efa84f053faf7eb14d2c104bef35b3098c23c5034678034c86ec183');
+    this.chain.push(genesisBlock)   //create Genesis Block
+}
+
+
+Blockchain.prototype.createNewBlock = function(pendingTransactions, previousHash){
+    var newBlock = new Block(this.getLastBlock().height+1, pendingTransactions, previousHash)
+
+    this.pendingTransactions = [];
+    this.chain.push(newBlock);
+
+    return newBlock;
+};
+
+Blockchain.prototype.getLastBlock = function(){
+    return this.chain[this.chain.length-1];
+};
+
+Blockchain.prototype.addTransactionToPendingTransaction = function(transactionObj){
+    this.pendingTransactions.push(transactionObj);
+    return this.getLastBlock()["height"]+1;
+};
+
+Blockchain.prototype.chainIsValid = function(blockchain){
+    var validChain = true;
+    for(var i=1; i<blockchain.length; i++){
+        const currentBlock = blockchain[i];
+        const prevBlock = blockchain[i-1];
+        const blockHash = this.hashBlock(prevBlock["hash"], {
+            transactions: currentBlock["transactions"],
+            index: currentBlock["height"]
+            },
+            currentBlock["nonce"]
+        );
+        if(blockHash.substring(0, 2) !== "00")
+            validChain = false;
+        if(currentBlock["previousBlockHash"] !== prevBlock["hash"])
+            validChain = false;
+    }
+
+    const genesisBlock = blockchain[0];
+    const correctNonce = genesisBlock["nonce"] === 100;
+    const correctPreviousBlockHash = genesisBlock["previousBlockHash"] === "0";
+    const correctHash = genesisBlock["hash"] === "0";
+    const correctTransactions = genesisBlock["transactions"].length === 0;
+
+    if(!correctNonce || !correctPreviousBlockHash || !correctHash || !correctTransactions)
+        validChain = false;
+    
+    return validChain;
+}
+
+Blockchain.prototype.getBlock = function(blockHash){
+
+    let correctBlock = null;
+    this.chain.forEach(block =>{
+        if(block.hash === blockHash)
+            correctBlock = block;
+    });
+
+    return correctBlock;
+};
+
+Blockchain.prototype.getTransaction = function(transactionId){
+
+    let correctTransaction = null;
+    let correctBlock = null
+    this.chain.forEach(block =>{
+        block.transactions.forEach(transaction =>{
+            if(transaction.transactionId == transactionId){
+                correctTransaction = transaction;
+                correctBlock = block;
+            }
+        });
+    });
+
+    return {
+        transaction: correctTransaction,
+        block: correctBlock
+    };
+};
+
+/*
+ *  This function (method) should be in wallet.js
+ */
+Blockchain.prototype.getAddressData = function(address){
+    const addressTransactions = [];
+    this.chain.forEach(block =>{
+        block.transactions.forEach(transaction =>{
+            if(transaction.sender === address || transaction.recipient === address)
+            addressTransactions.push(transaction);
+        });
+    });
+
+    let balance = 0;
+    addressTransactions.forEach(transaction =>{
+        if(transaction.recipient === address)
+            balance += transaction.amount;
+        if(transaction.sender === address)
+            balance -= transaction.amount;
+    });
+
+    return {
+        addressTransactions: addressTransactions,
+        addressBalance : balance
+    }
+
+}
+
+Blockchain.prototype.getBalance = function(publicKey) {
+    return this.accounts.getBalance(publicKey);
+}
+
+Blockchain.prototype.getLeader = function() {
+    return this.stakes.getMax(this.validators.list);
+}
+
+Blockchain.prototype.initialize = function(address) {
+    this.accounts.initialize(address);
+    this.stakes.initialize(address);
+}
+
+Blockchain.prototype.executeTransactions = function(block) {
+    block.data.forEach(transaction => {
+        switch (transaction.type) {
+            case TRANSACTION_TYPE.transaction:
+                this.accounts.update(transaction);
+                this.accounts.transferFee(block, transaction);
+            break;
+            case TRANSACTION_TYPE.stake:
+            this.stakes.update(transaction);
+            this.accounts.decrement(
+                transaction.input.from,
+                transaction.output.amount
+            );
+            this.accounts.transferFee(block, transaction);
+            break;
+            case TRANSACTION_TYPE.validator_fee:
+            if (this.validators.update(transaction)) {
+                this.accounts.decrement(
+                transaction.input.from,
+                transaction.output.amount
+                );
+                this.accounts.transferFee(block, transaction);
+            }
+            break;
+        }
+    });
+}
+
+Blockchain.prototype.executeChain = function(chain) {
+    chain.forEach(block => {
+        this.executeTransactions(block);
+    });
+}
+
+module.exports = Blockchain;
---
+++ wallet.js
@@ -0,0 +1,81 @@
+const sha256 = require("sha256");
+const secureRandom = require('secure-random');
+const elliptic = require('elliptic');
+const ripemd160 = require('ripemd160');
+const base58 = require('bs58');
+
+const CHECKSUM_LENGTH = 4; // 4 bytes
+
+function Wallet(prik='', pubk='', bal=0) {
+    this.balance = bal;
+    this.privateKey = prik;
+    this.publicKey = pubk;
+    if(prik == '' || pubk == ''){
+        this.NewKeyPair();
+    }
+};
+
+Wallet.prototype.NewKeyPair = function(){
+    this.privateKey = secureRandom.randomBuffer(32);
+    const ecdsa = new elliptic.ec('secp256k1');
+    const keys = ecdsa.keyFromPrivate(this.privateKey);
+    this.publicKey = keys.getPublic('hex');
+    //console.log("Private Key: ", privateKey);
+    //console.log("Public Key: ", publicKey);
+}
+
+Wallet.prototype.PublicKeyHash = function(){
+    let hash = sha256(Buffer.from(publicKey, 'hex'));
+    // console.log("Public Key:", publicKey)
+    let publicKeyHash = new ripemd160().update(Buffer.from(hash, 'hex')).digest();
+    // console.log(publicKeyHash.toString('hex'))
+    return publicKeyHash.toString('hex');
+}
+
+Wallet.prototype.Checksum = function(versionedHash){
+    var firstHash = sha256(Buffer.from(versionedHash, 'hex'));
+    var secondHash = sha256(Buffer.from(firstHash, 'hex'));
+    secondHash = Buffer.from(secondHash, 'hex');
+    return secondHash.subarray(0,CHECKSUM_LENGTH);
+
+}
+
+Wallet.prototype.Address = function() {
+    var pubHash = this.PublicKeyHash();
+    // console.log("PublicKeyHash: ", pubHash)
+    var versionedHash = "00" + pubHash;
+    // console.log("VersionedHash: ", versionedHash)
+    var checksum = this.Checksum(versionedHash);
+    // console.log("CHecksum: ", checksum);
+    var fullHash = versionedHash + checksum.toString('hex');
+    // console.log("FUllHash: ", fullHash)
+    
+    return base58.encode(Buffer.from(fullHash,'hex'));
+}
+
+Wallet.prototype.Sign = function(dataHash) {
+    const ecdsa = new elliptic.ec('secp256k1');
+    let signature = ecdsa.sign(dataHash, privateKey, "hex", {canonical:true});
+    return signature;
+}
+
+Wallet.prototype.ValidateAddress = function(address){
+    var pubHash = base58.decode(address);
+    // console.log("pubHash:", pubHash);
+    var actualChecksum = pubHash.subarray(pubHash.length - CHECKSUM_LENGTH);
+    // console.log("actualChecksum:", actualChecksum);
+    var version = pubHash.subarray(0,1);
+    var pubHash = pubHash.subarray(1,pubHash.length - CHECKSUM_LENGTH);
+    var targetChecksum = this.Checksum(Buffer.concat([version, pubHash]));
+    // console.log("targetChecksum:", targetChecksum)
+
+    return (actualChecksum.compare(targetChecksum) == 0)
+
+}
+
+module.exports = Wallet;
+
+// w = new Wallet();
+// console.log(w.Address(), base58.decode(w.Address()).toString('hex'));
+// console.log(w.ValidateAddress(w.Address()));
+// console.log((w.Sign("123")),(w.Sign("123")));
